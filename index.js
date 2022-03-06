b5f6a6fbc1a49ca0cfff287fdfb7efba458c62de
ManuelDev <manueldeveloper17@gmail.com>
2022-03-05 21:52:24 -0500

    Se agregaron los métodos GET, POST, PATCH, DELETE

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('Hola');
+// Importando express
+const express = require('express');
+// Importando modulo de rutas
+const routes = require('./routes/index.js');
+
+// Creando una instancia de express
+const app = express();
+
+// Permite recibir información en formato json
+app.use(express.json());
+
+// Puerto
+const port = 3000;
+// IP local, para probar en dispositivos de tu misma red
+const IP = "192.168.1.93";
+
+// Rutas
+routes(app);
+
+// Puerto en el que se va a escuchar el servidor
+app.listen(port, () => {
+    // console.log(`Server running on port: ${port}`);
+    console.log(`http://${IP}:${port}/`);
+});
---
+++ routes/products/products.router.js
@@ -0,0 +1,85 @@
+// Importando express
+const express = require('express');
+// Importando faker (Permite generar data fake)
+const faker = require('faker');
+
+const router = express.Router();
+
+// Rutas para productos
+/* LOS ENDPOINTS ESPECIFICOS DEBEN DECLARARSE ANTES DE LOS ENDPOINTS DINAMICOS!!! */
+
+// GET: Obtener todos los productos
+router.get('/', (req, res) => {
+    const products = [];
+    // Accediendo a los querys que vienen por URL
+    const { size } = req.query;
+    const limit = size || 10;
+
+    // Recorremos el array para agregar tantos productos queramos con faker
+    for (let i = 0; i < limit; i++) {
+        products.push({
+            // Generando datos con fake
+            name: faker.commerce.productName(),
+            price: parseInt(faker.commerce.price(), 10),
+            image: faker.image.imageUrl()
+        })
+    }
+
+    res.json(products);
+})
+
+router.get('/filter', (req, res) => {
+    res.send('Yo soy un filter');
+})
+
+// GET: Obtener un producto
+router.get('/:productId', (req, res) => {
+    // Accediendo a los parámetros que vienen por URL
+    const { productId } = req.params;
+
+    res.json({
+        productId,
+        name: 'Product 1',
+        price: 1000
+    })
+})
+
+// POST: Crear un producto
+router.post('/', (req, res) => {
+    // Accediendo a los datos que vienen por el body (Utilizar postman o insomnia)
+    const body = req.body;
+
+    res.json({
+        message: 'Product created',
+        data: body
+    })
+})
+
+// PATHC: Actualizar un producto
+router.patch('/:productId', (req, res) => {
+    // Accediendo a los parámetros que vienen por URL
+    const { productId } = req.params;
+
+    // Accediendo a los datos que vienen por el body (Utilizar postman o insomnia)
+    const body = req.body;
+
+    res.json({
+        message: 'Product updated',
+        data: body,
+        productId
+    })
+})
+
+// DELETE: Eliminar un producto
+router.delete('/:productId', (req, res) => {
+    // Accediendo a los parámetros que vienen por URL
+    const { productId } = req.params;
+
+    res.json({
+        message: 'Product deleted',
+        productId
+    })
+})
+
+// Exportamos módulo
+module.exports = router;
